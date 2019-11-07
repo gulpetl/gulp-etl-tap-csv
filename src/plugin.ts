bce91f9ac2f2b469875116189c8c2632d8b86068
Deepen Silwal <ernestosilwal@gmail.com>
2019-11-07 17:33:12 -0600

    commit with column_list_mode and normalize_column_names

--- src/plugin.ts
+++ src/plugin.ts
@@ -11,6 +11,9 @@ const ps = require('pause-stream')()
 const ReadableStreamClone = require("readable-stream-clone");
 
 const parse = require('csv-parse')
+import * as syncparse from 'csv-parse/lib/sync'
+
+
 
 /** wrap incoming recordObject in a Singer RECORD Message object*/
 function createRecord(recordObject:Object, streamName: string) : any {
@@ -43,7 +46,7 @@ async function getFirstLine (file:string) : Promise<string> {
   });
 }
 
-async function previewFirstLine (stream:any) : Promise<string> {
+async function previewFirstLineStream (stream:any) : Promise<string> {
   return new Promise((resolve, reject) => {
     let header : string = "";
     let allData : string = "";
@@ -55,7 +58,7 @@ async function previewFirstLine (stream:any) : Promise<string> {
         if (lines.length > 1 && header == "") {
           header = lines[0]
           stream.destroy();
-          // stream.pause()
+         // stream.pause()
           // stream.unpipe()
           resolve(header || allData)
         }
@@ -70,12 +73,153 @@ async function previewFirstLine (stream:any) : Promise<string> {
   });
 }
 
+function previewFirstLineBuffer(file: Vinyl){ 
+  let header;
+  if(file.isBuffer()){    
+      let tempStr = file.contents.toString();
+      header = tempStr.split(/\r?\n/)[0];
+  }
+  return header;
+}
+
+async function column_list_mode(this: any, configObj: any, file: Vinyl){
+  const self = this;
+  //configurations for column_list_mode
+  configObj.columns = false;
+  config_header_only(configObj)
+  let firstLine: any
+
+  if (file.isStream()) {
+    // grab first line of incoming file, which may be needed for some configObj options
+    try {
+      // let firstLine = await getFirstLine(file.path);
+      // zz = through2.obj().pipe(file.contents)
+      // zz = file.contents.pipe(ps.pause())
+      let z = new ReadableStreamClone(file.contents);
+      let zz = new ReadableStreamClone(file.contents);
+      let templine = await previewFirstLineStream(z)
+      let header = syncparse(templine)
+      firstLine = header[0]
+
+      log.debug(firstLine)
+      
+    }
+    catch (err) {
+      log.error(err);
+      self.emit('error', new PluginError(PLUGIN_NAME, err));
+    }
+  }
+
+  if(file.isBuffer()){
+    try{
+    
+     let templine: any = previewFirstLineBuffer(file)
+     let header = syncparse(templine)
+      // let tempStr = file.contents.toString();
+      // let header = tempStr.split(/\r?\n/)[0];
+      firstLine = header[0]
+
+    }
+    catch(err){
+      log.error(err);
+      self.emit('error', new PluginError(PLUGIN_NAME, err));
+    }
+  
+
+  }
+  console.log(firstLine);
+
+  return firstLine;
+}
+
+
+async function normalize_column_names(this: any, configObj: any, file: Vinyl){
+  const self = this;
+  config_header_only(configObj)
+  var normalized_header;
+
+  if (file.isStream()) {
+    // grab first line of incoming file, which may be needed for some configObj options
+    try {
+      // let firstLine = await getFirstLine(file.path);
+      // zz = through2.obj().pipe(file.contents)
+      // zz = file.contents.pipe(ps.pause())
+      let z = new ReadableStreamClone(file.contents);
+      let zz = new ReadableStreamClone(file.contents);
+      let firstLine = await previewFirstLineStream(z)
+      log.debug(firstLine)
+
+      normalized_header = regex_normalize_header(firstLine)
+      
+    }
+    catch (err) {
+      log.error(err);
+      self.emit('error', new PluginError(PLUGIN_NAME, err));
+    }
+  }
+
+  if(file.isBuffer()){
+    try{
+
+     let header = previewFirstLineBuffer(file)
+      // let tempStr = file.contents.toString();
+      // let header = tempStr.split(/\r?\n/)[0];
+  
+      normalized_header = regex_normalize_header(header)
+    }
+    catch(err){
+      log.error(err);
+      self.emit('error', new PluginError(PLUGIN_NAME, err));
+    }
+    
+
+    // let tempStr = file.contents.toString();
+    // let header = tempStr.split(/\r?\n/)[0];
+
+    // regex_normalize_header(header)
+
+    // let lineObj = syncparse(header) 
+
+    // for(let Index in lineObj[0]){
+    //   lineObj[0][Index] = lineObj[0][Index].replace(/[^a-zA-Z0-9]/g, "") as any;
+    // }
+    // configObj.columns = lineObj[0];
+    // console.log(configObj.columns)
+
+  }
+  console.log(normalized_header); 
+  return normalized_header;
+}
+
+function regex_normalize_header(header: any){
+  let lineObj = syncparse(header) 
+
+    for(let Index in lineObj[0]){
+      lineObj[0][Index] = lineObj[0][Index].replace(/[^a-zA-Z0-9]/g, "") as any;
+    }
+
+    // configObj.columns = lineObj[0];
+    console.log(lineObj[0])
+    return lineObj[0];
+}
+
+function config_header_only(configObj: any){
+  configObj.from = 0;
+  configObj.to = 1;
+}
+
+
+
+
 /* This is a gulp-etl plugin. It is compliant with best practices for Gulp plugins (see
 https://github.com/gulpjs/gulp/blob/master/docs/writing-a-plugin/guidelines.md#what-does-a-good-plugin-look-like ),
 and like all gulp-etl plugins it accepts a configObj as its first parameter */
 export function tapCsv(configObj: any) {
   if (!configObj) configObj = {}
   if (!configObj.columns) configObj.columns = true // we don't allow false for columns; it results in arrays instead of objects for each record
+  if (!configObj.column_list_mode) configObj.column_list_mode = false //if the falsey value is passed then it will assume the column_list_mode to be false
+  if (!configObj.normalize_column_names) configObj.normalize_column_names = false
+
 
   // creating a stream through which each file will pass - a new instance will be created and invoked for each file 
   // see https://stackoverflow.com/a/52432089/5578474 for a note on the "this" param
@@ -85,127 +229,129 @@ export function tapCsv(configObj: any) {
     let firstLine: string
     let zz
 
-    if (file.isStream()) {
-      // grab first line of incoming file, which may be needed for some configObj options
-      try {
-        // let firstLine = await getFirstLine(file.path);
-        // zz = through2.obj().pipe(file.contents)
-        // zz = file.contents.pipe(ps.pause())
-        let z = new ReadableStreamClone(file.contents);
-        zz = new ReadableStreamClone(file.contents);
-        let firstLine = await previewFirstLine(z)
-
-        log.debug(firstLine)
-      }
-      catch (err) {
-        log.error(err);
-        self.emit('error', new PluginError(PLUGIN_NAME, err));
-      }
-    }
-
-    const parser = parse(configObj)
-
-    // post-process line object
-    const handleLine = (lineObj: any, _streamName : string): object | null => {
-      if (parser.options.raw || parser.options.info) {
-        let newObj = createRecord(lineObj.record, _streamName)
-        if (lineObj.raw) newObj.raw = lineObj.raw
-        if (lineObj.info) newObj.info = lineObj.info
-        lineObj = newObj
-      }
-      else {
-        lineObj = createRecord(lineObj, _streamName)
-      }
-      return lineObj
-    }
-
-    function newTransformer(streamName : string) {
-
-      let transformer = through2.obj(); // new transform stream, in object mode
-  
-      // transformer is designed to follow csv-parse, which emits objects, so dataObj is an Object. We will finish by converting dataObj to a text line
-      transformer._transform = function (dataObj: Object, encoding: string, callback: Function) {
-        let returnErr: any = null
-        try {
-          let handledObj = handleLine(dataObj, streamName)
-          if (handledObj) {
-            let handledLine = JSON.stringify(handledObj)
-            log.debug(handledLine)
-            this.push(handledLine + '\n');
-          }
-        } catch (err) {
-          returnErr = new PluginError(PLUGIN_NAME, err);
-        }
-  
-        callback(returnErr)
-      }
-  
-      return transformer
-    }
-
-    // set the stream name to the file name (without extension)
-    let streamName : string = file.stem
-
-    if (file.isNull()) {
-      // return empty file
+    if(configObj.normalize_column_names){
+       let test = await normalize_column_names(configObj, file)
+       console.log(test)
+       file.contents = Buffer.from(test);
       return cb(returnErr, file)
+      //result is printed in the console but what left to do is dump it back to file
     }
-    else if (file.isBuffer()) {
+    else if(configObj.column_list_mode){
+      let test = await column_list_mode(configObj, file)
+      file.contents = Buffer.from(JSON.stringify(test));
+      console.log(test)
 
+      return cb(returnErr, file)
+      //result is printed in the console but what left to do is dump it back to file
+    } 
+    else {
+      const parser = parse(configObj)
 
-      parse(file.contents as Buffer, configObj, function(err:any, linesArray : []){
-        // this callback function runs when the parser finishes its work, returning an array parsed lines 
-        let tempLine: any
-        let resultArray = [];
-        // we'll call handleLine on each line
-        for (let dataIdx in linesArray) {
+      // post-process line object
+      const handleLine = (lineObj: any, _streamName : string): object | null => {
+        if (parser.options.raw || parser.options.info) {
+          let newObj = createRecord(lineObj.record, _streamName)
+          if (lineObj.raw) newObj.raw = lineObj.raw
+          if (lineObj.info) newObj.info = lineObj.info
+          lineObj = newObj
+        }
+        else {
+          lineObj = createRecord(lineObj, _streamName)
+        }
+        return lineObj
+      }
+  
+      function newTransformer(streamName : string) {
+  
+        let transformer = through2.obj(); // new transform stream, in object mode
+    
+        // transformer is designed to follow csv-parse, which emits objects, so dataObj is an Object. We will finish by converting dataObj to a text line
+        transformer._transform = function (dataObj: Object, encoding: string, callback: Function) {
+          let returnErr: any = null
           try {
-            let lineObj = linesArray[dataIdx]
-            tempLine = handleLine(lineObj, streamName)
-            if (tempLine){
-              let tempStr = JSON.stringify(tempLine)
-              log.debug(tempStr)
-              resultArray.push(tempStr);
+            let handledObj = handleLine(dataObj, streamName)
+            if (handledObj) {
+              let handledLine = JSON.stringify(handledObj)
+              log.debug(handledLine)
+              this.push(handledLine + '\n');
             }
           } catch (err) {
             returnErr = new PluginError(PLUGIN_NAME, err);
           }
+    
+          callback(returnErr)
         }
-        let data:string = resultArray.join('\n')
-
-        file.contents = Buffer.from(data)
-        
-        // we are done with file processing. Pass the processed file along
+    
+        return transformer
+      }
+  
+      // set the stream name to the file name (without extension)
+      let streamName : string = file.stem
+  
+      if (file.isNull()) {
+        // return empty file
+        return cb(returnErr, file)
+      }
+      else if (file.isBuffer()) {
+  
+  
+        parse(file.contents as Buffer, configObj, function(err:any, linesArray : []){
+          // this callback function runs when the parser finishes its work, returning an array parsed lines 
+          let tempLine: any
+          let resultArray = [];
+          // we'll call handleLine on each line
+          for (let dataIdx in linesArray) {
+            console.log(linesArray[dataIdx])
+            try {
+              let lineObj = linesArray[dataIdx]
+              tempLine = handleLine(lineObj, streamName)
+              if (tempLine){
+                let tempStr= JSON.stringify(tempLine)
+                log.debug(tempStr)
+                resultArray.push(tempStr);
+              }
+            } catch (err) {
+              returnErr = new PluginError(PLUGIN_NAME, err);
+            }
+          }
+          let data:string = resultArray.join('\n')
+  
+          file.contents = Buffer.from(data)
+          
+          // we are done with file processing. Pass the processed file along
+          log.debug('calling callback')    
+          cb(returnErr, file);    
+        })
+  
+      }
+      else if (file.isStream()) {
+        let zz = new ReadableStreamClone(file.contents);
+        file.contents = zz//file.contents
+          .pipe(parser)
+          .on('end', function () {
+  
+            // DON'T CALL THIS HERE. It MAY work, if the job is small enough. But it needs to be called after the stream is SET UP, not when the streaming is DONE.
+            // Calling the callback here instead of below may result in data hanging in the stream--not sure of the technical term, but dest() creates no file, or the file is blank
+            // cb(returnErr, file);
+            // log.debug('calling callback')    
+  
+            log.debug('csv parser is done')
+          })
+          // .on('data', function (data:any, err: any) {
+          //   log.debug(data)
+          // })
+          .on('error', function (err: any) {
+            log.error(err)
+            self.emit('error', new PluginError(PLUGIN_NAME, err));
+          })
+          .pipe(newTransformer(streamName))
+  
+        // after our stream is set up (not necesarily finished) we call the callback
         log.debug('calling callback')    
-        cb(returnErr, file);    
-      })
-
+        cb(returnErr, file);
+      }
     }
-    else if (file.isStream()) {
-      file.contents = zz//file.contents
-        .pipe(parser)
-        .on('end', function () {
 
-          // DON'T CALL THIS HERE. It MAY work, if the job is small enough. But it needs to be called after the stream is SET UP, not when the streaming is DONE.
-          // Calling the callback here instead of below may result in data hanging in the stream--not sure of the technical term, but dest() creates no file, or the file is blank
-          // cb(returnErr, file);
-          // log.debug('calling callback')    
-
-          log.debug('csv parser is done')
-        })
-        // .on('data', function (data:any, err: any) {
-        //   log.debug(data)
-        // })
-        .on('error', function (err: any) {
-          log.error(err)
-          self.emit('error', new PluginError(PLUGIN_NAME, err));
-        })
-        .pipe(newTransformer(streamName))
-
-      // after our stream is set up (not necesarily finished) we call the callback
-      log.debug('calling callback')    
-      cb(returnErr, file);
-    }
 
   })
 
